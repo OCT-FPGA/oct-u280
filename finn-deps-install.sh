08b5cfd13576e50956a979ad841931fa46ae1808
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2022-11-16 17:14:18 -0500

    Update finn-deps-install.sh

--- finn-deps-install.sh
+++ finn-deps-install.sh
@@ -3,7 +3,7 @@ sudo apt-get install -y python3-pip
 pip3 install bitstring
 pip3 install cffi
 pip3 install numpy
-pip3 install pynq
+pip3 install pynq==2.7.0
 pip3 install packaging
 
 wget https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
