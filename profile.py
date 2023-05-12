2c584432a505740093fa38aed9b5c8a7036c15dc
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2023-05-12 06:18:35 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -29,7 +29,7 @@ pc.defineParameter("nodeCount", "Number of Nodes", portal.ParameterType.INTEGER,
 # Pick your image.
 imageList = [
     ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD', 'UBUNTU 20.04'),    
-    ('urn:publicid:IDN+emulab.net+image+emulab-ops//CENTOS8-64-STD', 'CENTOS 8.4')] 
+    ('urn:publicid:IDN+emulab.net+image+emulab-ops//CENTOS8S-64-STD', 'CENTOS 8.4')] 
 
 toolVersion = [('2022.2'),
                ('Do not install tools')]      
