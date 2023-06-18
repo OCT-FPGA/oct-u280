9a4afd80e270a1f86ca1c1b5c00ec9a111362036
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2023-06-18 03:57:17 -0400

    Update post-boot.sh

--- post-boot.sh
+++ post-boot.sh
@@ -101,12 +101,19 @@ detect_cards() {
             yum install -y pciutils
         fi
     fi
-
-    for DEVICE_ID in $(lspci  -d 10ee: | grep " Processing accelerators" | grep "Xilinx" | grep ".0 " | cut -d" " -f7); do
-        if [[ "$DEVICE_ID" == "5008" ]] || [[ "$DEVICE_ID" == "d008" ]] || [[ "$DEVICE_ID" == "500c" ]] || [[ "$DEVICE_ID" == "d00c" ]]; then
-            U280=$((U280 + 1))
-        fi
-    done
+    if [[ "$OSVERSION" == "ubuntu-20.04" ]]; then
+        for DEVICE_ID in $(lspci  -d 10ee: | grep " Processing accelerators" | grep "Xilinx" | grep ".0 " | cut -d" " -f7); do
+            if [[ "$DEVICE_ID" == "5008" ]] || [[ "$DEVICE_ID" == "d008" ]] || [[ "$DEVICE_ID" == "500c" ]] || [[ "$DEVICE_ID" == "d00c" ]]; then
+                U280=$((U280 + 1))
+            fi
+        done
+    elif [[ "$OSVERSION" == "ubuntu-22.04" ]]; then
+        for DEVICE_ID in $(lspci  -d 10ee: | grep " Processing accelerators" | grep "Xilinx" | grep ".1 " | cut -d" " -f7); do
+            if [[ "$DEVICE_ID" == "5008" ]] || [[ "$DEVICE_ID" == "d008" ]] || [[ "$DEVICE_ID" == "500c" ]] || [[ "$DEVICE_ID" == "d00c" ]]; then
+                U280=$((U280 + 1))
+            fi
+        done
+    fi
 }
 
 verify_install() {
