599e106064d86da88aadc4fcda608a95fd110336
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2023-06-18 00:56:21 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -27,7 +27,8 @@ pc.defineParameter("nodeCount", "Number of Nodes", portal.ParameterType.INTEGER,
                    longDescription="Enter the number of FPGA nodes. Maximum is 16.")
 
 # Pick your image.
-imageList = [('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD', 'UBUNTU 20.04')] 
+imageList = [('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD', 'UBUNTU 20.04'),
+             ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD', 'UBUNTU 22.04')] 
 
 toolVersion = [('2023.1'),
                ('Do not install tools')]      
