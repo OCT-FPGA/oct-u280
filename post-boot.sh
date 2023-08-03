b48a2cc970f74d76360469cf288724cf2e60e7d0
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2023-08-03 11:47:07 -0400

    Update post-boot.sh

--- post-boot.sh
+++ post-boot.sh
@@ -25,6 +25,7 @@ install_xrt() {
         sudo yum install -y /tmp/$XRT_PACKAGE
     fi
     sudo bash -c "echo 'source /opt/xilinx/xrt/setup.sh' >> /etc/profile"
+    sudo bash -c "echo 'source /proj/octfpga-PG0/tools/Xilinx/Vitis/2023.1/settings64.sh' >> /etc/profile"
 }
 
 install_shellpkg() {
