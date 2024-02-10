febba09fc74bc40011cd39b555de1941972be7f4
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2024-02-09 19:49:17 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -106,6 +106,9 @@ for nodeName in nodeList:
     # Secret sauce.
     fpga.SubNodeOf(host)
 
+    host_iface1 = host.addInterface()
+    host_iface1.component_id = "eth2"
+    host_iface1.addAddress(pg.IPv4Address("192.168.40." + str(i+30), "255.255.255.0")) 
     fpga_iface1 = fpga.addInterface()
     fpga_iface1.component_id = "eth0"
     fpga_iface1.addAddress(pg.IPv4Address("192.168.40." + str(i+10), "255.255.255.0"))
@@ -115,7 +118,8 @@ for nodeName in nodeList:
     
     lan.addInterface(fpga_iface1)
     lan.addInterface(fpga_iface2)
-
+    lan.addInterface(host_iface1)
+  
     i+=1
 
 # Print Request RSpec
