07fcad12e99e59016f6676c2a563f6d58aec64f5
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2024-05-19 13:30:40 -0400

    Update post-boot.sh

--- post-boot.sh
+++ post-boot.sh
@@ -226,7 +226,7 @@ HOMEDIR="/users/$USER"
 echo "$HOMEDIR"
 
 #LOGFILE="/var/log/test_script.log"
-sudo -u $USER /local/repository/test.pl
+sudo -u $USER /local/repository/cold-reboot.pl
 
 sudo -u $USER /local/repository/test.sh
 
