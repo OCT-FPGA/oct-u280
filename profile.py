c56fc41a721f67e78afdd4f1e1044c01fd7be610
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2025-02-27 18:41:36 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -42,10 +42,6 @@ imageList = [('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD', '
 workflow = ['Vitis', 'Vivado']
 
 toolVersion = ['2023.1', '2023.2'] 
-
-pc.defineParameter("nodes","List of nodes",
-                   portal.ParameterType.STRING,"",
-                   longDescription="Comma-separated list of nodes (e.g., pc151,pc153). Please check the list of available nodes within the Mass cluster at https://www.cloudlab.us/cluster-status.php before you specify the nodes.")
                    
 pc.defineParameter("workflow", "Workflow",
                    portal.ParameterType.STRING,
@@ -60,27 +56,7 @@ pc.defineParameter("osImage", "Select Image",
                    portal.ParameterType.IMAGE,
                    imageList[0], imageList,
                    longDescription="Supported operating systems are Ubuntu and CentOS.")  
-
-# Optional ephemeral blockstore
-pc.defineParameter("tempFileSystemSize", "Temporary Filesystem Size",
-                   portal.ParameterType.INTEGER, 0,advanced=True,
-                   longDescription="The size in GB of a temporary file system to mount on each of your " +
-                   "nodes. Temporary means that they are deleted when your experiment is terminated. " +
-                   "The images provided by the system have small root partitions, so use this option " +
-                   "if you expect you will need more space to build your software packages or store " +
-                   "temporary files.")
-# Instead of a size, ask for all available space. 
-pc.defineParameter("tempFileSystemMax",  "Temp Filesystem Max Space",
-                    portal.ParameterType.BOOLEAN, False,
-                    advanced=True,
-                    longDescription="Instead of specifying a size for your temporary filesystem, " +
-                    "check this box to allocate all available disk space. Leave the size above as zero.")
-
-pc.defineParameter("tempFileSystemMount", "Temporary Filesystem Mount Point",
-                   portal.ParameterType.STRING,"/mydata",advanced=True,
-                   longDescription="Mount the temporary file system at this mount point; in general you " +
-                   "you do not need to change this, but we provide the option just in case your software " +
-                   "is finicky.")  
+ 
                    
 # Retrieve the values the user specifies during instantiation.
 params = pc.bindParameters()        
@@ -89,63 +65,15 @@ params = pc.bindParameters()
   
 pc.verifyParameters()
 
-lan = request.LAN()
-
-nodeList = params.nodes.split(',')
-i = 0
-for nodeName in nodeList:
-    host = request.RawPC(nodeName)
-    # UMass cluster
-    host.component_manager_id = "urn:publicid:IDN+cloudlab.umass.edu+authority+cm"
-    # Assign to the node hosting the FPGA.
-    host.component_id = nodeName
-    host.disk_image = params.osImage
-
-    vm1 = mkVM(host, "vm1");
+host = request.RawPC("host")
+host.hardware_type = "fpga-alveo"
+host.disk_image = params.osImage
+vm1 = mkVM(host, "vm1");
     
-    # Optional Blockstore
-    if params.tempFileSystemSize > 0 or params.tempFileSystemMax:
-        bs = host.Blockstore(nodeName + "-bs", params.tempFileSystemMount)
-        if params.tempFileSystemMax:
-            bs.size = "0GB"
-        else:
-            bs.size = str(params.tempFileSystemSize) + "GB"
-        bs.placement = "any"
-
-    #host.addService(pg.Execute(shell="bash", command="sudo /local/repository/post-boot.sh " + params.workflow + " " + params.toolVersion + " >> /local/logs/output_log.txt"))
-
-    # Since we want to create network links to the FPGA, it has its own identity.
-    #fpga = request.RawPC("fpga-" + nodeName)
-    # UMass cluster
-    #fpga.component_manager_id = "urn:publicid:IDN+cloudlab.umass.edu+authority+cm"
-    # Assign to the fgpa node
-    #fpga.component_id = "fpga-" + nodeName
-    # Use the default image for the type of the node selected. 
-    #fpga.setUseTypeDefaultImage()
-
-    # Secret sauce.
-    #fpga.SubNodeOf(host)
-
-    #host_iface1 = host.addInterface("br0")
-    #host_iface1.component_id = "eth2"
-    #host_iface1.addAddress(pg.IPv4Address("192.168.40." + str(i+30), "255.255.255.0")) 
-
-    #vm_iface = vm.addInterface("br0")  
-    #vm_iface.addAddress(pg.IPv4Address("192.168.40." + str(i+10), "255.255.255.0"))
-  
-    #fpga_iface1 = fpga.addInterface()
-    #fpga_iface1.component_id = "eth0"
-    #fpga_iface1.addAddress(pg.IPv4Address("192.168.40." + str(i+10), "255.255.255.0"))
-    #fpga_iface2 = fpga.addInterface()
-    #fpga_iface2.component_id = "eth1"
-    #fpga_iface2.addAddress(pg.IPv4Address("192.168.40." + str(i+20), "255.255.255.0"))
     
-    #lan.addInterface(fpga_iface1)
-    #lan.addInterface(fpga_iface2)
-    #lan.addInterface(host_iface1)
-    #lan.addInterface(vm_iface)
-  
-    i+=1
+#host.addService(pg.Execute(shell="bash", command="sudo /local/repository/post-boot.sh " + params.workflow + " " + params.toolVersion + " >> /local/logs/output_log.txt"))
+
+
 
 # Print Request RSpec
 pc.printRequestRSpec(request)
