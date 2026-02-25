a0348f5c7fdd36d00df7e341a5faba98b44cee74
Pavani Kuppili <41535888+pavanikuppili@users.noreply.github.com>
2026-02-24 19:34:21 -0700

    Comment out host_iface2 configurations
    Comment out unused host interface configurations for clarity.

--- profile.py
+++ profile.py
@@ -126,11 +126,11 @@ for nodeName in nodeList:
         host_iface1 = host.addInterface()
         host_iface1.component_id = "eth2"
         host_iface1.addAddress(pg.IPv4Address("192.168.40." + str(n_idx+10), "255.255.255.0"))
-        host_iface2 = host.addInterface()
-        host_iface2.component_id = "eth3"
-        host_iface2.addAddress(pg.IPv4Address("192.168.40." + str(n_idx+20), "255.255.255.0"))
+        # host_iface2 = host.addInterface()
+        # host_iface2.component_id = "eth3"
+        # host_iface2.addAddress(pg.IPv4Address("192.168.40." + str(n_idx+20), "255.255.255.0"))
         lan1.addInterface(host_iface1)
-        lan1.addInterface(host_iface2)
+        #lan1.addInterface(host_iface2)
         
         fpga_iface1 = fpga.addInterface()
         fpga_iface1.component_id = "eth0"
@@ -142,13 +142,13 @@ for nodeName in nodeList:
         lan2.addInterface(fpga_iface2)
     else: #p4 fpga smartnic
         host_iface1 = host.addInterface()
-        host_iface2 = host.addInterface()
+        # host_iface2 = host.addInterface()
         host_iface1.component_id = "eth2"
-        host_iface2.component_id = "eth3"
+        # host_iface2.component_id = "eth3"
         host_iface1.addAddress(pg.IPv4Address("192.168.50." + str(n_idx+30), "255.255.255.0"))
-        host_iface2.addAddress(pg.IPv4Address("192.168.50." + str(n_idx+40), "255.255.255.0"))
+        # host_iface2.addAddress(pg.IPv4Address("192.168.50." + str(n_idx+40), "255.255.255.0"))
         lan2.addInterface(host_iface1)
-        lan2.addInterface(host_iface2)
+        # lan2.addInterface(host_iface2)
 
         fpga_iface1 = fpga.addInterface()
         fpga_iface1.component_id = "eth0"
