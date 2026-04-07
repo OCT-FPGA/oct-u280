89252d0c0da6ddd73cd9092cce5352cf57bbba97
pontarelli <salvatore.pontarelli@uniroma1.it>
2026-04-07 19:33:37 +0200

    Add new image option for UBUNTU 22.04 on cloudlab

--- profile.py
+++ profile.py
@@ -18,7 +18,8 @@ request = pc.makeRequestRSpec()
 
 # Pick your image.
 imageList = [('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD', 'UBUNTU 22.04'),
-             ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD', 'UBUNTU 20.04')] 
+             ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD', 'UBUNTU 20.04'),
+             ('urn:publicid:IDN+cloudlab.umass.edu+image+octfpga-PG0:oct-u280.pc163.mcs', 'UBUNTU 22.04 +mcs ')] 
 
 workflow = ['Vitis', 'Vivado']
 
