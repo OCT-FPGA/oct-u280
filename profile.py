516b7f88c5c28c681d47a2f93b984c49e05c3dd6
Pavani Kuppili <41535888+pavanikuppili@users.noreply.github.com>
2026-07-01 20:23:52 -0500

    Modify loopback server interface configuration
    Comment out the addition of host_iface1 and its configuration, while enabling host_iface2 for the loopback server.

--- profile.py
+++ profile.py
@@ -123,14 +123,14 @@ for nodeName in nodeList:
     fpga.SubNodeOf(host)
 
     if n_idx == 0: #loopback server
-        host_iface1 = host.addInterface()
-        host_iface1.component_id = "eth2"
-        host_iface1.addAddress(pg.IPv4Address("192.168.40." + str(n_idx+10), "255.255.255.0"))
-        # host_iface2 = host.addInterface()
-        # host_iface2.component_id = "eth3"
+        # host_iface1 = host.addInterface()
+        # host_iface1.component_id = "eth2"
+        #host_iface1.addAddress(pg.IPv4Address("192.168.40." + str(n_idx+10), "255.255.255.0"))
+        host_iface2 = host.addInterface()
+        host_iface2.component_id = "eth3"
         # host_iface2.addAddress(pg.IPv4Address("192.168.40." + str(n_idx+20), "255.255.255.0"))
-        lan1.addInterface(host_iface1)
-        #lan1.addInterface(host_iface2)
+        #lan1.addInterface(host_iface1)
+        lan1.addInterface(host_iface2)
         
         fpga_iface1 = fpga.addInterface()
         fpga_iface1.component_id = "eth0"
